ed9bf47fa7bcafeb76d4ceb3c45d65e26db2c194
Karl-E. Kiel <kekiel@kekiel.de>
2015-02-06 21:02:11 +0100

    Replaced ListView by new RecyclerView

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -3,14 +3,15 @@ package com.example.android.sunshine.app;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -63,8 +64,58 @@ public class MainActivity extends ActionBarActivity {
      */
     public static class PlaceholderFragment extends Fragment {
 
+        List<String> mWeekForecast;
+
         public PlaceholderFragment() {
         }
+
+        /**
+         * A ViewHolder to be used by the new ForecastAdapter.
+         * Holds references to the items in the list elements.
+         */
+        private class ForecastViewHolder extends RecyclerView.ViewHolder {
+
+            private TextView mItemTextView;
+            private String mForecastString;
+
+            // constructor fetches the TextView reference to store it in a member variable
+            public ForecastViewHolder(View itemView) {
+                super(itemView);
+                mItemTextView = (TextView) itemView.findViewById(R.id.list_item_forecast_textview);
+            }
+
+            // will be used by the adapter to apply a value to the TextView
+            public void bindForecast(String forecast) {
+                mForecastString = forecast;
+                mItemTextView.setText(forecast);
+            }
+        }
+
+        /**
+         * Adapter for the RecyclerView.
+         * Binds the values of mWeekForecast array to the list_item_forecast view.
+         */
+        private class ForecastAdapter extends RecyclerView.Adapter<ForecastViewHolder> {
+            @Override
+            public ForecastViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+                // when initially created, inflate the view and init the ViewHolder
+                View view = LayoutInflater.from(getActivity().getApplicationContext()).inflate(R.layout.list_item_forecast, viewGroup, false);
+                return new ForecastViewHolder(view);
+            }
+
+            @Override
+            public void onBindViewHolder(ForecastViewHolder forecastViewHolder, int i) {
+                // each time an item is to be displayed, make use of the ViewHolder to set the value
+                String forecastString = mWeekForecast.get(i);
+                forecastViewHolder.bindForecast(forecastString);
+            }
+
+            @Override
+            public int getItemCount() {
+                return mWeekForecast.size();
+            }
+        }
+
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -79,23 +130,21 @@ public class MainActivity extends ActionBarActivity {
                     "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
                     "Sun 6/29 - Sunny - 20/7"
             };
-            List<String> weekForecast = new ArrayList<String>(Arrays.asList(data));
+            mWeekForecast = new ArrayList<>(Arrays.asList(data));
 
-            // Now that we have some dummy forecast data, create an ArrayAdapter.
-            // The ArrayAdapter will take data from a source (like our dummy forecast) and
-            // use it to populate the ListView it's attached to.
-            ArrayAdapter<String> forecastAdapter =
-                    new ArrayAdapter<String>(
-                            getActivity(), // The current context (this activity)
-                            R.layout.list_item_forecast, // The name of the layout ID.
-                            R.id.list_item_forecast_textview, // The ID of the textview to populate.
-                            weekForecast);
+            // Now that we have some dummy forecast data, create an Adapter for the RecyclerView.
+            // The ForecastAdapter will take data from a source (like our dummy forecast) and
+            // use it to populate the RecyclerView it's attached to.
 
+            // inflate the basic view containing the RecyclerView element
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-
-            // Get a reference to the ListView, and attach this adapter to it.
-            ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
-            listView.setAdapter(forecastAdapter);
+            // Get a reference to the RecyclerView, and attach a  adapter to it.
+            RecyclerView recyclerView = (RecyclerView) rootView.findViewById(R.id.recyclerview_forecast);
+            // attach the adapter
+            recyclerView.setAdapter(new ForecastAdapter());
+            // A RecyclerView.Adapter needs a LayoutManager
+            // We simply use a LinearLayout, provided by the framework
+            recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
             return rootView;
         }
